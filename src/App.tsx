651fe5c92ec4546f859231dd302e4c7f89679c0c
Warllei Martins <warllei.s.martins@hotmail.com>
2025-03-10 16:41:23 -0300

    fix: correções

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
 import NavigationConfig from "./navigation/navigation";
 
 const App = () => {
   return (
-    <BrowserRouter>
       <NavigationConfig />
-    </BrowserRouter>
   );
 };
 
--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -6,26 +6,30 @@ import { CiMenuFries } from "react-icons/ci";
 import { FaWhatsapp } from "react-icons/fa";
 import Layout from "./layout";
 import "./style.scss";
+import { BrowserRouter } from "react-router-dom";
 
 const NavigationConfig = () => {
     return (
-        <div className="router-container">
-            <div className="sidebar-mobile-toggler">
-                <CiMenuFries size={40} />
+        <BrowserRouter>
+            <div className="router-container">
+                <div className="sidebar-mobile-toggler">
+                    <CiMenuFries size={40} />
+                </div>
+                <div className="sidebar">
+                    <Layout />
+                </div>
+                <div className="main-content">
+                    <Routes>
+                        <Route path="/" element={<AtasRegister />}>
+                            <Route index element={<AtasRegister />} />
+                            <Route path="listOneAtaRegister" element={<ListOneAtaRegister />} />
+                        </Route>
+                    </Routes>
+                </div>
+                <Button className="support-button"><FaWhatsapp /> Suporte</Button>
             </div>
-            <div className="sidebar">
-                <Layout />
-            </div>
-            <div className="main-content">
-                <Routes>
-                    <Route path="/" element={<AtasRegister />}>
-                        <Route index element={<AtasRegister />} />
-                        <Route path="listOneAtaRegister" element={<ListOneAtaRegister />} />
-                    </Route>
-                </Routes>
-            </div>
-            <Button className="support-button"><FaWhatsapp /> Suporte</Button>
-        </div>
+        </BrowserRouter>
+
     );
 };
 
